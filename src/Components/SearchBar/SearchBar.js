5392e87c191cb3b18877716cddf0889f8fa78ae4
Elena Soboleva <sobolevalena21@gmail.com>
2022-03-11 14:33:57 -0500

    Initial commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    };
+    
+    this.search = this.search.bind(this);
+    this.handleTermChange=this.handleTermChange.bind(this);
+  }
+  
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  handleTermChange(e) {
+    this.setState({term: e.target.value});
+  }
+
+  render() {
+    return (
+        <div className="SearchBar">
+            <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"  />
+            <button className="SearchButton" onClick={this.search}>SEARCH</button>
+        </div>
+    )
+  }
+}
+
+export default SearchBar;
